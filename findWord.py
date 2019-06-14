80f7e1cb0a763c2040691e06ac6772febb777aab
prashantpy <51730905+prashantpy@users.noreply.github.com>
2019-06-14 08:04:12 +0530

    Update findWord.py

--- findWord.py
+++ findWord.py
@@ -1,29 +1,38 @@
+wordFoundFlag = False
 
-def checkWordExists(grid, word, i, j):
-    k = 0
-    print(i,j)
-    if word[0] == grid[i][j]:
-        print(word[0])
-    if (i < 0 or j < 0 or i > 3 or j > 3):
-        return False
-    if word[k] == grid[i][j]:
-        k += 1;
-        print(grid[i][j])
-        if checkWordExists(grid, word, i - 1, j) or checkWordExists(grid, word, i + 1, j) or checkWordExists(grid, word, i, j + 1) or checkWordExists(grid, word, i, j - 1):
-
-            return True
-        else:
+def checkWordExists(grid, word, i, j, k):
+    global wordFoundFlag
+    if wordFoundFlag == False:
+        if (i < 0 or j < 0 or i > 3 or j > 3):
             return False
+        if grid[i][j] == '*':
+            return False
+        if k < len(word) and word[k] == grid[i][j]:
+            k += 1
+            grid[i][j] = '*';
+            if k == len(word):
+                wordFoundFlag = True;
+            if k <= len(word):
+                if (checkWordExists(grid, word, i - 1, j, k) or checkWordExists(grid, word, i + 1, j, k)
+                             or checkWordExists(grid, word, i, j + 1, k) or checkWordExists(grid, word, i, j - 1, k)):
+                                 return True
+                else:
+                    return False
+    return wordFoundFlag
 
 
 if __name__ == '__main__':
 
-    _grid = [list("axmy"), list("bgdf"), list("xcet"), list("rass")]
-    _word = "geeks"
+    _grid = [list("axmy"), list("bgdf"), list("xeet"), list("raks")]
+    _word = "geeksa"
     row = 4
     col = 4
+    k = 0
 
     for i in range(0, row):
         for j in range(0, col):
-            checkWordExists(_grid, _word, i, j)
+            if wordFoundFlag == False:
+                checkWordExists(_grid, _word, i, j, k)
+    print(wordFoundFlag)
+                
     
